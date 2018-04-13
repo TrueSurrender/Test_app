e180c27861f7a229d5477a049386d8c4d85032ab
TrueSurrender <sannetbodewes@gmail.com>
2018-04-13 06:04:04 -0400

    Fix create, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,28 @@
 class ArticlesController < ApplicationController
   
+  def index
+  end
+  
   def new
     @article = Article.new
   end
   
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
   
   def article_params
     params.require(:article).permit(:title, :description)
-  end
-  
-  
+  end 
 end
